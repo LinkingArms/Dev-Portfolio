fc0d408089549f200b689ee87f4892b23f0a0869
LinkingArms <135238376+LinkingArms@users.noreply.github.com>
2024-04-01 20:27:16 -0400

    Started Tweaking Menu
    Started work on adding dynamic functionality to the site menu, and making the menu actually switch between pages.

--- site-script.js
+++ site-script.js
@@ -1,67 +1,103 @@
 function LoadArticle() {
-    xmlRequest = new XMLHttpRequest();
-    xmlRequest.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            BuildArticle(this.responseXML);
-        }
-    };
-    xmlRequest.open("GET", `articles/${document.getElementsByTagName("article")[0].dataset.article}.xml`, true);
-    xmlRequest.send();
+  xmlParser = new DOMParser();
+  fetch(
+    `articles/${
+      document.getElementsByTagName("article")[0].dataset.article
+    }.xml`
+  )
+    .then((file) => file.text())
+    .then((article) =>
+      BuildArticle(xmlParser.parseFromString(article, "text/xml"))
+    );
 }
 
 function BuildArticle(articleDoc) {
-    body = document.getElementsByTagName("article")[0];
-    content = Array.from(articleDoc.documentElement.childNodes).filter((node) => node.nodeType == 1);
-    for (const item of content) {
-        switch (item.nodeName) {
-            case "title":
-                body.append(document.createElement("h1"));
-                body.childNodes[body.childNodes.length - 1].append(item.innerHTML);
-                break;
-        
-            case "section":
-                body.append(document.createElement("h2"));
-                body.childNodes[body.childNodes.length - 1].append(item.innerHTML);
-                break;
-                
-            case "paragraph":
-                body.append(document.createElement("p"));
-                body.childNodes[body.childNodes.length - 1].append(
-                    document.createElement("span"),
-                    item.innerHTML
-                );
-                body.childNodes[body.childNodes.length - 1].childNodes[0].classList.add("paragraph-indent");
-                break;
-            
-            default:
-                console.error(`${item.nodeName}is not a valid node type.`);
-                break;
-        }
+  body = document.getElementsByTagName("article")[0];
+  content = Array.from(articleDoc.documentElement.childNodes).filter(
+    (node) => node.nodeType == 1
+  );
+  for (const item of content) {
+    switch (item.nodeName) {
+      case "title":
+        body.append(document.createElement("h1"));
+        body.childNodes[body.childNodes.length - 1].append(item.innerHTML);
+        break;
+
+      case "section":
+        body.append(document.createElement("h2"));
+        body.childNodes[body.childNodes.length - 1].append(item.innerHTML);
+        break;
+
+      case "paragraph":
+        body.append(document.createElement("p"));
+        body.childNodes[body.childNodes.length - 1].append(
+          document.createElement("span"),
+          item.innerHTML
+        );
+        body.childNodes[body.childNodes.length - 1].childNodes[0].classList.add(
+          "paragraph-indent"
+        );
+        break;
+
+      default:
+        console.error(`${item.nodeName} is not a valid node type.`);
+        break;
     }
+  }
 }
 
-function IndentAll() {
-    let paragraphs = document.getElementsByTagName("article")[0].getElementsByTagName("p");
-    for (let i = 0; i < paragraphs.length; i++) {
-        document.getElementsByTagName("article")[0].getElementsByTagName("p")[i].prepend(
-            document.createElement("span")
-        );
-        document.getElementsByTagName("article")[0].getElementsByTagName("p")[i].getElementsByTagName("span")[0].classList.add("paragraph-indent");
+function LoadArticleList() {
+  fetch("articles.json")
+    .then((file) => file.json())
+    .then((articleList) => BuildArticleList(articleList));
+}
+
+function BuildArticleList(articleList) {
+  menu = document.getElementsByTagName("nav")[0];
+  menu.append(document.createElement("ul"));
+  for (const item of articleList.categories) {
+    menu.childNodes[1].append(document.createElement("li"));
+    if (item.content.length == 1) {
+      menu.childNodes[1].childNodes[
+        menu.childNodes[1].childNodes.length - 1
+      ].append(document.createElement("button"));
+      menu.childNodes[1].childNodes[
+        menu.childNodes[1].childNodes.length - 1
+      ].childNodes[0].append(item.content[0].title);
+    } else {
+        
     }
+  }
 }
 
 function openMenu(elementID) {
-    document.getElementById(elementID).classList.toggle(elementID + "-hidden");
-    document.getElementById(elementID).classList.toggle(elementID + "-visible");
-    document.getElementsByTagName("article")[0].classList.toggle(`article-${elementID}-hidden`);
-    document.getElementsByTagName("article")[0].classList.toggle(`article-${elementID}-visible`);
-    document.getElementsByTagName("header")[0].classList.toggle(`header-${elementID}-hidden`);
-    document.getElementsByTagName("header")[0].classList.toggle(`header-${elementID}-visible`);
-    document.getElementById("header-fade").classList.toggle(`header-${elementID}-hidden`);
-    document.getElementById("header-fade").classList.toggle(`header-${elementID}-visible`);
-    document.getElementById("footer-fade").classList.toggle(`footer-${elementID}-hidden`);
-    document.getElementById("footer-fade").classList.toggle(`footer-${elementID}-visible`);
+  document.getElementById(elementID).classList.toggle(elementID + "-hidden");
+  document.getElementById(elementID).classList.toggle(elementID + "-visible");
+  document
+    .getElementsByTagName("article")[0]
+    .classList.toggle(`article-${elementID}-hidden`);
+  document
+    .getElementsByTagName("article")[0]
+    .classList.toggle(`article-${elementID}-visible`);
+  document
+    .getElementsByTagName("header")[0]
+    .classList.toggle(`header-${elementID}-hidden`);
+  document
+    .getElementsByTagName("header")[0]
+    .classList.toggle(`header-${elementID}-visible`);
+  document
+    .getElementById("header-fade")
+    .classList.toggle(`header-${elementID}-hidden`);
+  document
+    .getElementById("header-fade")
+    .classList.toggle(`header-${elementID}-visible`);
+  document
+    .getElementById("footer-fade")
+    .classList.toggle(`footer-${elementID}-hidden`);
+  document
+    .getElementById("footer-fade")
+    .classList.toggle(`footer-${elementID}-visible`);
 }
 
 LoadArticle();
-IndentAll();
+LoadArticleList();
